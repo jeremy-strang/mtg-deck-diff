8a96b1bb859592d62264b2aa7ffae9e36b978751
jeremy-strang <jeremiahstrang@gmail.com>
2022-08-02 16:06:25 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,137 @@
+import React from 'react'
+import './App.scss'
+import { Deck, DeckComparer } from './helpers/diff'
+
+interface IProps {
+  deckListStrA: string
+  deckListStrB: string
+}
+
+interface IState {
+  deckListStrA: string
+  deckListStrB: string
+  diffListStr: string
+}
+
+class App extends React.Component<IProps, IState> {
+  private deckInputA: React.RefObject<HTMLTextAreaElement>
+  private deckInputB: React.RefObject<HTMLTextAreaElement>
+  private diffOutput: React.RefObject<HTMLTextAreaElement>
+
+  constructor(props: IProps) {
+    super(props)
+
+    this.deckInputA = React.createRef<HTMLTextAreaElement>()
+    this.deckInputB = React.createRef<HTMLTextAreaElement>()
+    this.diffOutput = React.createRef<HTMLTextAreaElement>()
+
+    let deckListStrA = window.localStorage ? window.localStorage.getItem('deckListStrA') || '' : ''
+    this.state = {
+      deckListStrA: deckListStrA,
+      deckListStrB: '',
+      diffListStr: '',
+    } as IState
+  }
+
+  componentDidUpdate = () => {
+    this.resetScroll()
+  }
+
+  resetScroll = () => {
+    if (this.deckInputA && this.deckInputA.current) {
+      this.deckInputA.current.scrollTop = 0
+    }
+    if (this.deckInputB && this.deckInputB.current) {
+      this.deckInputB.current.scrollTop = 0
+    }
+    if (this.diffOutput && this.diffOutput.current) {
+      this.diffOutput.current.scrollTop = 0
+    }
+    this.diffOutput.current?.focus()
+  }
+
+  computeDiffListStr = (deckListStrA: string, deckListStrB: string): string => {
+    const deckListA = new DeckComparer(deckListStrA)
+    return deckListA.computeDiff(new DeckComparer(deckListStrB)).toString()
+  }
+
+  handleChangeA = (e: any) => {
+    const deckListStrA = e.target.value || ''
+    if (window.localStorage) {
+      window.localStorage.setItem('deckListStrA', deckListStrA)
+    }
+    if (this.state.deckListStrB) {
+      this.setState({
+        deckListStrA,
+        diffListStr: this.computeDiffListStr(e.target.value, this.state.deckListStrB),
+      }, () => this.resetScroll())
+    } else {
+      this.setState({
+        deckListStrA,
+      })
+    }
+  }
+
+  handleChangeB = (e: any) => {
+    if (this.state.deckListStrA) {
+      this.setState({
+        deckListStrB: e.target.value,
+        diffListStr: this.computeDiffListStr(this.state.deckListStrA, e.target.value),
+      })
+    } else {
+      this.setState({
+        deckListStrB: e.target.value,
+      })
+    }
+  }
+
+  render(): React.ReactNode {
+    let resultWrap: React.ReactNode
+
+    if (this.state.deckListStrA && this.state.deckListStrB) {
+      resultWrap = <div className='Width100'>
+        <div className='DeckListWrap'>
+          <h4>Results</h4>
+          <textarea
+            className='DeckList'
+            value={this.state.diffListStr}
+            ref={this.diffOutput}
+            rows={15}
+            readOnly={true}></textarea>
+        </div>
+      </div>
+    } else {
+      resultWrap = <div className='Width100'>
+        <h4 className='TextCenter'>Add two decklists to compute their differences</h4>
+      </div>
+    }
+    return (
+      <div className="App">
+        <div className='Width100'>
+          <div className='DeckListWrap'>
+            <h4>Deck A</h4>
+            <textarea
+              className='DeckList'
+              value={this.state.deckListStrA}
+              onChange={this.handleChangeA}
+              ref={this.deckInputA}
+              rows={15}></textarea>
+          </div>
+
+          <div className='DeckListWrap'>
+            <h4>Deck B</h4>
+            <textarea
+              className='DeckList'
+              value={this.state.deckListStrB}
+              ref={this.deckInputB}
+              onChange={this.handleChangeB}
+              rows={15}></textarea>
+          </div>
+        </div>
+        {resultWrap}
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/helpers/diff.ts
@@ -0,0 +1,137 @@
+export class Deck {
+  companion: DeckLine[]
+  mainDeck: DeckLine[]
+  sideboard: DeckLine[]
+
+  constructor(companion?: DeckLine[], mainDeck?: DeckLine[], sideboard?: DeckLine[]) {
+    this.companion = companion ?? []
+    this.mainDeck = mainDeck ?? []
+    this.sideboard = sideboard ?? []
+  }
+
+  static parse(deckListStr: string): Deck {
+    const parsedDeck = new Deck([], [], [])
+
+    if (deckListStr) {
+      let mainDone = false
+      let companionDone = true
+      for (let line of deckListStr.trim().split('\n').map(l => l.trim().replace(/[\t]/g, ' '))) {
+        if (line === 'Companion') {
+          companionDone = false
+        }
+
+        if (!companionDone && (['', 'Deck', 'Sideboard'].indexOf(line) > -1)) {
+          companionDone = true
+        } else if (line === '' || line === 'Sideboard') {
+          mainDone = true
+        }
+
+        if (/^\d+\s+.*$/.test(line)) {
+          let deckLine: DeckLine = {
+            quantity: parseInt(line.slice(0, line.indexOf(' '))),
+            card: line.slice(line.indexOf(' '), line.length).trim(),
+          }
+          
+          if (!companionDone) {
+            parsedDeck.companion.push(deckLine)
+          } else if (!mainDone) {
+            parsedDeck.mainDeck.push(deckLine)
+          } else {
+            parsedDeck.sideboard.push(deckLine)
+          }
+        }
+      }
+    }
+
+    return parsedDeck
+  }
+
+  toString(): string {
+    let result = ''
+
+    const joinDeckLines = (deckLines: DeckLine[]) =>
+      deckLines.map(item => `${item.quantity > 0 ? '+' : ''}${item.quantity} ${item.card}`).join('\n')
+
+    if (this.companion.length > 0) {
+      const companionStr = joinDeckLines(this.companion)
+      result += `Companion\n${companionStr}\n\n`
+    }
+    if (this.mainDeck.length > 0) {
+      const mainStr = joinDeckLines(this.mainDeck)
+      result += `Deck\n${mainStr}\n\n`
+    }
+    if (this.sideboard.length > 0) {
+      const sideStr = joinDeckLines(this.sideboard)
+      result += `Sideboard\n${sideStr}\n\n`
+    }
+    return result
+  }
+}
+
+export interface DeckLine {
+  quantity: number
+  card: string
+}
+
+export class DeckComparer {
+  deck: Deck
+
+  constructor(deckListStr: string) {
+    this.deck = Deck.parse(deckListStr)
+  }
+
+  computeDiff(otherDeckList: DeckComparer): Deck {
+    const otherDeck = otherDeckList.deck
+
+    const diff = new Deck(
+      this.computeSubDiff(this.deck.companion, otherDeck.companion),
+      this.computeSubDiff(this.deck.mainDeck, otherDeck.mainDeck),
+      this.computeSubDiff(this.deck.sideboard, otherDeck.sideboard))
+    
+    console.log(diff)
+    return diff
+  }
+
+  private buildCardQuantityMap(items: DeckLine[]): Map<string, number> {
+    const result = new Map<string, number>()
+    for (let item of items) {
+      if (result.has(item.card)) {
+        result.set(item.card, item.quantity + (result.get(item.card) ?? 0))
+      } else {
+        result.set(item.card, item.quantity)
+      }
+    }
+    return result
+  }
+
+  private computeSubDiff(thisDeck: DeckLine[], otherDeck: DeckLine[]): DeckLine[] {
+    const thisDeckMap = this.buildCardQuantityMap(thisDeck)
+    const otherDeckMap = this.buildCardQuantityMap(otherDeck)
+    const diffDeckMap = new Map<string, number>()
+
+    for (let cq of thisDeck) {
+      let otherQuantity = otherDeckMap.has(cq.card) ? otherDeckMap.get(cq.card) : 0
+      if (otherQuantity !== cq.quantity) {
+        diffDeckMap.set(cq.card, (otherQuantity ?? 0) - cq.quantity)
+      }
+    }
+
+    for (let cq of otherDeck) {
+      let thisQuantity = thisDeckMap.has(cq.card) ? thisDeckMap.get(cq.card) : 0
+      if (thisQuantity !== cq.quantity && !diffDeckMap.has(cq.card)) {
+        diffDeckMap.set(cq.card, cq.quantity - (thisQuantity ?? 0))
+      }
+    }
+
+    const subDiff: DeckLine[] = []
+
+    for (let pair of Array.from(diffDeckMap)) {
+      subDiff.push({
+        quantity: pair[1],
+        card: pair[0],
+      })
+    }
+
+    return subDiff
+  }
+}
